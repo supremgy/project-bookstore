06c7be71f18850816926e3ee5ccc77c24b1f564c
supremgy <supremgy@icloud.com>
2024-01-04 22:40:26 +0900

    category table query setting

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,32 +1,48 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
+//(카테고리별, 신간 여부 ) 전체 도서 목록 조회
 const allBooks = (req, res) => {
-  const { category_id } = req.query;
-  if (category_id) {
-    //카테고리별 도서 조회
-    let sql = 'SELECT * FROM books WHERE category_id = ?';
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
-  } else {
-    let sql = 'SELECT * FROM books';
-    conn.query(sql, (err, results) => {
-      if (err) {
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
+  const { category_id, news, limit, currentPage } = req.query;
+  //limit : page page 당 도 서 수
+  // currentPage : 현재  몇 페이지
+  //offset : limit * (currentPage-1)
+  let offset = limit * (currentPage - 1);
+  let sql = 'select * from books ';
+  let values = [];
+  if (category_id && news) {
+    sql +=
+      'WHERE category_id = ? AND pub_date between date_sub(now(), interval 1 month) and now()';
+    values = [category_id];
+  } else if (category_id) {
+    sql += 'WHERE category_id = ?';
+    values = [category_id];
+  } else if (news) {
+    sql += 'where pub_date between date_sub(now(), interval 1 month) and now()';
   }
+  sql += ' LIMIT ? OFFSET ?';
+  values.push(parseInt(limit), offset);
+  //카테고리별 도서 조회
+  console.log('sql', sql);
+  console.log('values', values);
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end();
+    }
+  });
 };
 
 const bookDetail = (req, res) => {
   let { id } = req.params;
   id = parseInt(id);
-  let sql = 'SELECT * FROM books WHERE id = ?';
+  let sql =
+    'select * from books left join category on books.category_id = category.id WHERE books.id = ?;';
   conn.query(sql, id, (err, results) => {
     if (err) {
       console.log(err);
